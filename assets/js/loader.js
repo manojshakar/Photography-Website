726c3c2a25647083bb31eb2660ab146d7fa3820c
Manoj Shakar <manojshakar.com@gmail.com>
2020-04-21 14:50:11 +0530

    Changes

---
+++ assets/js/loader.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function loadDone(){
+	setTimeout(function(){
+		document.getElementById("loading_holder").style.display = "none";
+		document.getElementById("fullsingle").style.display = "block";
+	}, 500);
+	
+}
